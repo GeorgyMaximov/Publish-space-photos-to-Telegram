7e4b668d07d0986fd51353143cf7aa9f86f07d61
GeorgyMaximov <117512148+GeorgyMaximov@users.noreply.github.com>
2023-05-16 18:18:06 +0300

    Update get_epic_images.py

--- get_epic_images.py
+++ get_epic_images.py
@@ -11,6 +11,7 @@ def get_epic_images(nasa_api_token):
         "api_key": nasa_api_token
     }
     response = requests.get(url, params=payload)
+    response.raise_for_status()
     for number_image, features_image in enumerate(response.json()):
         date_image = features_image["date"]
         date = datetime.datetime.fromisoformat(date_image).date().strftime("%Y/%m/%d")
